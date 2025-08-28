f7c3497f02de2ef56b806f1e0e36c409457f8b7e
luigui29 <luiguigarrido29@gmail.com>
2025-08-28 16:47:09 -0400

    feat: picture gallery shows images and generic image in case of null

--- public/js/app.js
+++ public/js/app.js
@@ -6,13 +6,16 @@ async function allCreatures() {
     
     main.insertAdjacentHTML("afterbegin", `
         <div class="outer-container">
-            <div class="text-container" id="loading">
-                <h1>Fetching the Creature Collection! Please wait...</h1>
+            <div class="showcase-container">
+                <div class="text-container" id="loading">
+                    <h1>Fetching the Creature Collection! Please wait...</h1>
+                </div>
             </div>
         </div>
     `);
 
     let outer_container = document.querySelector('.outer-container');
+    let showcase_container = document.querySelector('.showcase-container');
     let loading = document.querySelector('#loading');
     
     /* FETCH DATA FROM SERVER */
@@ -27,7 +30,7 @@ async function allCreatures() {
         loading.style.display = "none";
 
         if (creatures.length === 0) { // -- Check for empty array of creatures.
-            outer_container.insertAdjacentHTML("afterbegin", `
+            showcase_container.insertAdjacentHTML("afterbegin", `
                 <div class="text-container">
                     <h1>You open the encyclopedia, only to find that it's wiped...</h1>
                 </div>
@@ -36,16 +39,28 @@ async function allCreatures() {
         }
 
         creatures.forEach(creature => {
-            outer_container.insertAdjacentHTML("beforeend", `
-                <div class="text-container">
-                    <h1>Creature ${creature.id}: ${creature.name}</h1>
+            let image_folder = creature.name ?
+                creature.name.toLowerCase() : `unknown` 
+            
+            showcase_container.insertAdjacentHTML("beforeend", `
+                <div class="creature-card">
+                    <div class="creature-card-img">
+                        <img 
+                            src = "../images/picture_gallery/${image_folder}/${image_folder.replace(/$/, "_1.png")}"
+                            onerror = "this.onerror = null; this.src = '../images/picture_gallery/unknown/unknown_1.png'"
+                            alt = "Sprite of ${creature.name}"
+                        >
+                    </div>
+                    <div class="text-container">
+                        <h1><span> Creature ${creature.id} </span> <br> ${creature.name}</h1>
+                    </div>
                 </div>
             `);
         });
 
     } catch (error) { 
         loading.style.display = "none";
-        outer_container.insertAdjacentHTML("afterbegin", `
+        showcase_container.insertAdjacentHTML("afterbegin", `
             <div class="text-container" id="errors">
                 <h1>The creatures are missing... Come back when we find them.</h1>
             </div>
@@ -56,4 +71,3 @@ async function allCreatures() {
 }
 
 allCreatures();
-
