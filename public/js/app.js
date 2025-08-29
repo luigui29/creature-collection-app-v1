1c4f8d79708a79f84382aa0bbd86e74d90017e35
luigui29 <luiguigarrido29@gmail.com>
2025-08-29 15:36:58 -0400

    feat: refactored the app.js fetch to use more .then handlers (try and catch error doesn't work)

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 /*Step-by-Step for the app.js*/
 /*TODO: Pass this to the README.md*/
 let main = document.querySelector('main');
+const fallback_image_path = `../images/picture_gallery/unknown/unknown_1.png`;
 
 
 function loadImage(src) {
@@ -11,6 +12,40 @@ function loadImage(src) {
         img.onerror = () => reject(src);
         img.src = src;
     })
+}
+
+async function createCreatureCards(image_load_promises, creature_data, showcase_container){
+    await loadImage(fallback_image_path);
+    Promise.allSettled(image_load_promises)
+        .then((response) => {
+            const fragment = document.createDocumentFragment();
+
+            creature_data.forEach((creature, index) => {
+                let image_source = (response[index].status === `fulfilled`)
+                ? response[index].value
+                : fallback_image_path;
+
+                /* Defining the Elements to be built inside fragment */
+                let creature_card = document.createElement(`div`);
+                creature_card.className = `creature-card`;
+                creature_card.innerHTML = `
+                    <div class="creature-card-img">
+                        <img 
+                            src = "${image_source}"
+                            alt = "Sprite of ${creature.name || 'Unknown'}"
+                        >
+                    </div>
+                    <div class="text-container">
+                        <h1><span> Creature ${creature.id} </span> <br> ${creature.name || 'Unknown'}</h1>
+                    </div>
+                
+                `
+                fragment.appendChild(creature_card);
+            });
+                
+            loading.style.display = "none";
+            showcase_container.appendChild(fragment);
+        })
 }
 
 async function allCreatures() {
@@ -30,66 +65,38 @@ async function allCreatures() {
     
     /* FETCH DATA FROM SERVER */
     try {
-        const response = await fetch('/api/creatures'); // -- Should return JSON with all data from creatures.
-        if (!response.ok) { throw new Error(`Server error: ${response.status}`); }
-        const creatures = await response.json(); // -- Parse JSON into JS array.
+        const fetch_response = await fetch('/api/creatures') // -- Should return JSON with all data from creatures.
+            .then((response) => {
 
-        /* Case: Creatures JSON is empty */
-        if (creatures.length === 0) { // -- Check for empty array of creatures.
-            showcase_container.insertAdjacentHTML("afterbegin", `
-                <div class="text-container">
-                    <h1>You open the encyclopedia, only to find that it's wiped...</h1>
-                </div>
-            `);
-            return;
-        }
+                if (!response.ok) { 
+                    throw new Error(`Server error: ${response.status}`); 
+                }
 
-        /* Await all creature image data, map to image_load_promises */
-        const image_load_promises = creatures.map(creature => {
-            const image_name = creature.name 
-            ? creature.name.toLowerCase() 
-            : `unknown`;
+                const creatures = response.json();
 
-            const ideal_image_path = `../images/picture_gallery/${image_name}/${image_name}_1.png`
-            return loadImage(ideal_image_path);
-        });
-        const image_load_results = await Promise.allSettled(image_load_promises);
-        
-        console.log(`image_load_promises` , image_load_promises);
-        console.log(`image_load_results` , image_load_results);
-        
-        /* Build in-memory element containing all cards that will be sent to DOM once fully loaded */
-        const fragment = document.createDocumentFragment();
-        const fallback_image_path = `../images/picture_gallery/unknown/unknown_1.png`;
-        await loadImage(fallback_image_path);
+                creatures.then((data) => {
+                    if (data.length === 0) { // -- Check for empty array of creatures.
+                        showcase_container.insertAdjacentHTML("afterbegin", `
+                            <div class="text-container">
+                                <h1>You open the encyclopedia, only to find that it's wiped...</h1>
+                            </div>
+                        `);
+                    
+                        return;
+                    }
 
-        creatures.forEach((creature, index) => {
-            let image_source = (image_load_results[index].status === `fulfilled`)
-            ? image_load_results[index].value
-            : fallback_image_path;
+                    const image_load_promises = data.map(creature => {
+                        const image_name = creature.name 
+                        ? creature.name.toLowerCase() 
+                        : `unknown`;
 
-            /* Defining the Elements to be built inside fragment */
-            let creature_card = document.createElement(`div`);
-            creature_card.className = `creature-card`;
-            creature_card.innerHTML = `
-                <div class="creature-card-img">
-                    <img 
-                        src = "${image_source}"
-                        alt = "Sprite of ${creature.name || 'Unknown'}"
-                    >
-                </div>
-                <div class="text-container">
-                    <h1><span> Creature ${creature.id} </span> <br> ${creature.name || 'Unknown'}</h1>
-                </div>
-            
-            `
+                        const ideal_image_path = loadImage(`../images/picture_gallery/${image_name}/${image_name}_1.png`);
+                        return ideal_image_path;
+                    });
 
-            fragment.appendChild(creature_card);
-        });
-
-        showcase_container.appendChild(fragment);
-        loading.style.display = "none";
-
+                    createCreatureCards(image_load_promises, data, showcase_container);                    
+                });
+            });
     } catch (error) { 
         loading.style.display = "none";
         showcase_container.insertAdjacentHTML("afterbegin", `
